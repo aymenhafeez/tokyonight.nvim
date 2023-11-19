310a0dfd85b7a0d8295d7e1b1d8fe90d5d1d990b
aymenhafeez <aymennh@gmail.com>
2023-11-19 22:21:40 +0000

    changes

--- lua/tokyonight/theme.lua
+++ lua/tokyonight/theme.lua
@@ -429,7 +429,7 @@ function M.setup()
     NeoTreeNormalNC = { fg = c.fg_sidebar, bg = c.bg_sidebar },
     NeoTreeDimText = { fg = c.fg_gutter },
     -- NeoTreeWinSeparator = { fg = c.bg, bg = c.bg },
-    NeoTreeWinSeparator = { fg = c.fg_sidebar, bg = c.bg_sidebar },
+    NeoTreeWinSeparator = { fg = c.bg_sidebar, bg = c.bg_sidebar },
     NeoTreeCursorLine = { bg = c.bg_highlight_2 },
 
     -- Fern
